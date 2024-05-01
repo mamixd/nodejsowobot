a12a709c580527f50aa2e59ff0e591a5b0b5367c
mamixd <103064438+mamixd@users.noreply.github.com>
2024-05-01 20:55:33 +0300

    Delete HuntBot directory

--- HuntBot/commands/durdur.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-    info: "Anında programı kapatır.",
-    callback: (message, ...args) => {
-        message.reply("Kapatılıyor...")
-        process.kill(process.pid, "SIGINT");
-    }
-}
--- HuntBot/commands/say.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-    info: "komutları yerine getirmesini/bir şey söylemesini sağlayın.",
-    callback: (message, ...args) => {
-        message.channel.send(args.join(" "));
-    }
-}
